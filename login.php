8812c9c25cf011459d452f7ee3fa27420af1d986
gioescobar9 <giovanni.escobar@bobcats.gcsu.edu>
2016-04-04 13:06:04 -0400

    Update login.php

--- login.php
+++ login.php
@@ -9,8 +9,24 @@ if($_SESSION["lFail"] == true)
 echo "<h3>Incorrect username or password</h3>";
 
 echo<<<_END
-<form action = "login.Buffer.php" method = 'post
+<form action = "login.Buffer.php" method = 'post'>
+<fieldset>
+<legend>Login:</login>
+Username: <br>
+<input type: "email" name "username" value ""><br>
+Password: <br>
+<input type = "password" name = "pwd" value = ""><br><br>
+<input type = "submit" value = "Login">
+
+</fieldset>
+</form>
 
 _END;
 
+/* we set lFail  back to false so when we navigate elsewhere and come back to this page the login error message will be gone */
+
+$_SESSION["lFail"] = false;
+
 ?>
+</body>
+</html>
