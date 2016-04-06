ff63ec647c3acb7dbd2c7dab68ad007738045ab1
bthrasher0815 <brian.thrasher@bobcats.gcsu.edu>
2016-04-06 11:24:18 -0400

    Update headerFooter.php

--- headerFooter.php
+++ headerFooter.php
@@ -1,4 +1,5 @@
 <?php
+include 'style.php'
 /* page header function to include on every page to display the same lay out of the page
 includes a link to the login page, the products page, and the who we are page*/
 function pageHeader($pt)
@@ -14,22 +15,24 @@ _END;
 }
 
 /*generate table function used throughout our project to generate a table when needed*/
-	function generateTable($arr, $colNames)
-	{
+function generateTable($arr, $colNames)
+{
 		
-		echo"<table>";
-		echo"<tr>";
-		foreach($colNames as $col)
-		{
-			echo "<th>$col</th>";
-		}
-		echo "</tr>";
-		foreach($arr as $record)
-		{
-			echo "<tr>";
-			foreach($record as $col)
-				echo "<td> $col</td>";
-			echo "</tr>";
-		}
-		echo "</table>";
+	echo"<table>";
+	echo"<tr>";
+	foreach($colNames as $col)
+	{
+		echo "<th>$col</th>";
 	}
+	echo "</tr>";
+	foreach($arr as $record)
+	{
+	echo "<tr>";
+		foreach($record as $col)
+		{
+		echo "<td> $col</td>";
+		echo "</tr>";
+		}
+	echo "</table>";
+	}
+}
