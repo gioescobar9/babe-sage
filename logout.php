bd953722abb7bfefc2053610883ce597c5ae4d17
bthrasher0815 <brian.thrasher@bobcats.gcsu.edu>
2016-04-06 12:24:00 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,20 @@
+<?php
+
+session_start();
+$_SESSION["login"] = false;
+unset($_SESSION['admin]);
+
+setcookie("user",NULL, time() -864000);
+setcookie("user",NULL, time() -864000);
+setcookie("user",NULL, time() -864000);
+setcookie("user",NULL, time() -864000);
+setcookie("user",NULL, time() -864000);
+setcookie("user",NULL, time() -864000);
+
+$redirect_page = "index.php";
+$redirect = true;
+if($redirect == true)
+header('location:'.$redirect_page);
+
+?>
 
