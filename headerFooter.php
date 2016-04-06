37114b88b17db1ae2b54207da7f30542c955ed63
bthrasher0815 <brian.thrasher@bobcats.gcsu.edu>
2016-04-06 12:07:11 -0400

    Update headerFooter.php

--- headerFooter.php
+++ headerFooter.php
@@ -7,11 +7,16 @@ function pageHeader($pt)
 echo<<<_END;
   <h1><img src="imageName" width="200" height="140" alt="farmLogo" align="left"/><br>$pt</h1>
   <h3>
-  <a href = "login.php"><Login</a>
-  <a href = "products.php"><Products</a>
-  <a href = "about.php"><Who we are</a>
-  </h3>
+  <a href = "index.php">Home</a>
+  <a href = "products.php">Products</a>
+  <a href = "about.php">Who we are</a>
+  
 _END;
+if($_SESSION["login"] == true)
+	echo"<a href = 'login.php'>Login</a>";
+if($_SESSION["login"] == false)
+	echo"<a href = 'logout.php'>Logout</a>";
+	echo "</h3>";
 }
 
 /*generate table function used throughout our project to generate a table when needed*/
