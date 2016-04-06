6ae47363d2586f73f34cad01be6a0e83a6450bd5
bthrasher0815 <brian.thrasher@bobcats.gcsu.edu>
2016-04-06 12:46:40 -0400

    Update clear.php

--- clear.php
+++ clear.php
@@ -1,1 +1,15 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+session_write_close();
+secookie(session_name(),'','/');
+session_regenerate_id(true);
+
+$redirect_page = "products.php";
+$redirect = true;
+
+if($redirect == true)
+header('location:'.$redirect_page);
+?>
 
