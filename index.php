2cb0509990d33530bd8c1c970403735c24abd61d
Promethicon <Promethicon@gmail.com>
2016-04-20 12:41:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once "headerFooter.php";
+pageHeader("Home");
+echo("<h1>Welcome to Babe and Sage Farm! </h1>
+echo("<p1>Babe and Sage farm is a produce farm based out of Gordon County, Georgia. We produce vegetables, 
+          bread, eggs, and a variety of other produce. We are a local farm that participates in the Commmunity 
+          Supported Agriculture (CSA) system, which allows customers to purhase a portion of each week's crop in
+          advance.</p1>")
+echo("<p2>Here is what's on offer this week: </p2>")          
+?>
