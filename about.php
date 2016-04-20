b417d7c692630bb6a5bcd084223f7e06879683dd
Promethicon <Promethicon@gmail.com>
2016-04-20 13:33:19 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php
+require_once "headerFooter.php";
+echo<<<_END
+<h1>About Us</h1>
+		
+<p id = "people">Babe and Sage farm is based in Gordon, Georgia, and is run by Bobby and Chelsea
+		Losh-Jones. Both Bobby and Chelsea graduated from Georgia College and State University
+		in nearby Milledgeville, Georgia. They founded the farm in 2011, and had their first child,
+		Tripp, in 2014.</p>
+		
+<h2 id = "property">About the Property</h2>
+				
+<p id = "house"> The farm itself is 28 acres, 10 fallow and 18 of pecans. Situated on the farmstead
+		is a renovated 1800s farmhouse. The farm itself is part of a larger farm owned by the 
+		Oetter family of Macon since the Great Depression. Babe and Sage LLC holds a long-term lease
+		on the farm portion of the property.</p>
+		
+<h2 id = practices> About our Practices</h2>
+
+<p id = "organic">The farm is not certified organic, but strives to use organic farming methods,
+		as they follow all the USDA's regulations for certified organic, meaning that they do not
+		utilize conventional pesticides, fertilizers made from synthetic ingredients, sewage sludge,
+		bioengineering, or ionizing radiation. They view the farm as a whole system, and seek to
+		make it as sustainable as possible. </p>
+
+<h2 id = "mission statement">Our mission statement</h2>
+
+<p id = "mission"> Our mission is to provide customers in Middle Georgia with fresh seasonal produce
+		and seasonal farm products of the highest quality; to manage the land in an economically,
+		socially, and environmentally stable way. </p>		
+_END
+?>
