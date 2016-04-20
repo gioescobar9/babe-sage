d6599566735d177144ef77dcc22919661f441a62
bthrasher0815 <brian.thrasher@bobcats.gcsu.edu>
2016-04-20 14:41:48 -0400

    Update produce.php

--- produce.php
+++ produce.php
@@ -1,3 +1,50 @@
 <?php
-
+// include dbOperations and headerFooter which prevents repeating the same code in each file 
+require_once 'dbOperations.php';
+require_once 'headerFooter.php';
+pageHeader("Weekly Share + Yearly Produce")
+//connect to the database and check for successful connection
+$connection = connectDB();
+if($connection->connect_error) die("unable to connect to database".$connection->connect_error);
+//query to select produce based on the availablity and submit the query as long as it doesnt fail
+$query="SELECT name, description FROM products WHERE availability != 0"
+$result = $connection->query($query)
+if(!$result) die ("Query failed".$connection->connect_error);
+$rows=$result->num_rows;
+echo "<h2>This weeks produce includes: </h2>"
+echo "<br>"
+//for loop used to display the available produce 
+for($i=0; $i<$rows; $i++)
+{
+  echo "<p>";
+  $result->data_seek($i);
+  $record=$result->fetch_array(MYSQLI_ASSOC);
+  $productName = $record["name"];
+  $descrip = $record["description"];
+  echo "<h3>".$productName."</h3>"
+  echo "<h4>".$descrip."</h4>"
+  echo "</p>"
+}
+echo "<br>"
+// new query to show yearly produce, also checking to make sure the query was successful
+$queryNew="SELECT name, description FROM products"
+$resultNew = $connection->query($queryNew)
+if(!$result) die ("Query failed".$connection->connect_error);
+$newRows=$resultNew->num_rows;
+echo "<h2>This weeks produce includes: </h2>"
+echo "<br>"
+//for loop to display all products names with heading 3 and all descriptions with heading 4
+for($i=0; $i<$newRows; $i++)
+{
+  echo "<p>";
+  $resultNew->data_seek($i);
+  $record=$resultNew->fetch_array(MYSQLI_ASSOC);
+  $productName = $record["name"];
+  $descrip = $record["description"];
+  echo "<h3>".$productName."</h3>"
+  echo "<h4>".$descrip."</h4>"
+  echo "</p>"
+}
+//close connection
+closeConnection($result, $connection);
 ?>
