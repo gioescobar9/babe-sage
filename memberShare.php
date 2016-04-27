b8362d0a1df1dc3241f26b8f3b5ba2beaaca9a69
bthrasher0815 <brian.thrasher@bobcats.gcsu.edu>
2016-04-27 13:34:04 -0400

    Update memberShare.php

--- memberShare.php
+++ memberShare.php
@@ -1,28 +1,49 @@
 <?php
-require_once 'dbOperations.php';
+session_start();
 require_once 'headerFooter.php';
-pageHeader("Your Weekly Share")
+require_once 'dbOperations.php';
+
+pageHeader("Your Weekly Share");
+$cols = 3;
+
 //connect to the database and check for successful connection
 $connection = connectDB();
 if($connection->connect_error) die("unable to connect to database".$connection->connect_error);
-//query to select produce based on the availablity and submit the query as long as it doesnt fail
-$query="SELECT name, description, price FROM products WHERE availability != 0";
-$result = $connection->query($query)
+
+$query="SELECT name, description,imageName,price FROM products WHERE availability != 0";
+$result = $connection->query($query);
 if(!$result) die ("Query failed".$connection->connect_error);
 $rows=$result->num_rows;
-echo "<h2>This weeks produce includes: </h2>";
+echo "<h2>This Weeks Box </h2>";
 echo "<br>";
-//for loop used to display the available produce 
-for($i=0; $i<$rows; $i++)
+//for loop used to create a table of produce that is available 
+echo "<table>";
+for($i=0;$i<$rows;$i++)
 {
-  $result->data_seek($i);
-  $record=$result->fetch_array(MYSQLI_ASSOC);
-  $productName = $record["name"];
-  $descrip = $record["description"];
-  echo "<h3>".$productName."</h3>";
-  echo "<p>";
-  echo $descrip;
-  echo "</p>";
+	if($i%$cols ==0)
+	echo "<tr>";
+	$result->data_seek($i);
+	$record=$result->fetch_array(MYSQLI_ASSOC);
+	
+	$vName =$record["name"];
+	$imgN = "images/".$record["imageName"];
+	$desc = $record["description"];
+	$price = $record["price"];
+	
+	echo "<td>";
+	//this will display the image along with name and description;
+	echo<<<_END
+	
+	<img src = "$imgN" alt = "$vName" title = "$desc"height = "200" width = "300">
+	<h4>$vName."-".$price</h4>
+			
+
+_END;
+	echo "</td> ";
+	if($i%$cols==3)
+	echo"</tr>";
 }
-echo "<br>"
+echo "</table>";
+
+echo "<br>";
 ?>
